c0c93bc2414efd4bcb13b016f9511977def70aa4
Hossein Mohasel Arjomandi <hm31@chackoge-serv01.cs.illinois.edu>
2023-11-12 21:41:56 -0600

    Added data cleaning to sample case

--- step_3_generate_tables/pyspark_parquet.py
+++ step_3_generate_tables/pyspark_parquet.py
@@ -3,6 +3,7 @@ from pyspark.sql.functions import col
 from pyspark.sql import functions as func
 from pyspark.sql.types import StringType
 import time
+import os
 
 
 def insert_table(result, jdbc_url, jdbc_properties):
@@ -13,10 +14,10 @@ def insert_table(result, jdbc_url, jdbc_properties):
 
 
 spark = SparkSession.builder \
-    .appName("parquet_unification") \
-    .config("spark.executor.memory", "10g") \
-    .getOrCreate()
-# spark.conf.set("spark.executor.memory", "4g")
+        .appName("parquet_unification") \
+        .config("spark.executor.memory", "10g") \
+        .getOrCreate()
+    # spark.conf.set("spark.executor.memory", "4g")
 
 
 parquet_path = '/shared/hossein_hm31/pubmed_parquet/'
@@ -25,32 +26,47 @@ parquet_path = '/shared/hossein_hm31/test/'
 df = spark.read.parquet(parquet_path)
 spark.sparkContext.setLogLevel("WARN")
 
-
 df.printSchema()
 start = time.time()
 
-# df = df.repartition(2)
 df = df.repartition(1)
-
 df = df.dropDuplicates(['doi'])
-
-
 init_count = df.count()
+mid = time.time()
 
-print(df.count(),'asoon! drop duplicates', time.time() - start)
-# df.show(10)
+print(df.count(),'asoon! drop duplicates', mid - start)
+df.createOrReplaceTempView("temp_table")
 
-# jdbc_url = "jdbc:postgresql://valhalla.cs.illinois.edu:5432/ernieplus"
-# jdbc_properties = {
-#     "user": "hm31",
-#     "password": "graphs",
-#     "driver": "org.postgresql.Driver"
-# }
-# mid = time.time()
-# insert_table(df,jdbc_url,jdbc_properties)
-# df.write.parquet("/shared/hossein_hm31/whole.parquet")
+sql_query = """
+       WITH RankedRows AS (
+           SELECT
+               *,
+               ROW_NUMBER() OVER (PARTITION BY PMID ORDER BY date_completed DESC) AS RowRank
+           FROM
+               temp_table
+       )
+       SELECT
+           *
+       FROM
+           RankedRows
+       WHERE
+           RowRank = 1
+"""
+
+result_df = spark.sql(sql_query)
+result_df = result_df.drop("RowRank")
+
+    # result_df = result_df.drop("RowRank")
 end = time.time()
-print(f'elapsed {end - start} partition {df.rdd.getNumPartitions()}')
+
+print(f'finalllll {end - mid} count: {result_df.count()}')
+
 
 spark.stop()
+
+    # Add a delay (adjust the duration as needed)
+
+    # Print a message to indicate the script has completed
+print("Script completed successfully.")
+
 

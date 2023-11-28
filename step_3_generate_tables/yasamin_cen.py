945df7d648d5e248c462f2444776262c51cc5714
Hossein Mohasel Arjomandi <hm31@chackoge-serv01.cs.illinois.edu>
2023-11-28 10:20:30 -0600

    Refactored code and added cen_nodes

--- step_3_generate_tables/yasamin_cen.py
+++ step_3_generate_tables/yasamin_cen.py
@@ -8,21 +8,28 @@ from pyspark.sql.functions import col
 
 import sys
 
+#Read the CEN from the tsv file
 def read_CEN(file_path, spark):
     schema = StructType([StructField("PMID", IntegerType(), False), StructField("cluster_id", IntegerType(), False)])
     cr = spark.read.schema(schema).option("sep", " ").csv(file_path)
     return cr
 
 
+#Read a df from db
 def read_df(spark, jdbc_url, table_name, jdbc_properties ):
     df = spark.read.jdbc(url=jdbc_url, table=table_name, properties=jdbc_properties)
     return df
 
 
+#Using George_pipeline that maps dois and PMIDS, we use the dimensions_complete_nodelist to get the subset
+#intersection of dimensions and baseline that have full metadata
 def calculate_node_id_to_pmid_mapping(cen_nodes, george_df, full = False):
     dimensions_df =  read_df(spark, jdbc_url, 'dimensions.exosome_dimensions_complete_nodelist', jdbc_properties)
     george_df.createOrReplaceTempView("george_pipeline")
     dimensions_df.createOrReplaceTempView("exosome_dimensions")
+
+    print("george partition", george_df.rdd.getNumPartitions())
+    print("dimensions_df partition", dimensions_df.rdd.getNumPartitions())
 
     if full == False: #Take with partial details
         # Register DataFrames as temporary tables to be used in SQL queries
@@ -41,13 +48,14 @@ def calculate_node_id_to_pmid_mapping(cen_nodes, george_df, full = False):
 
     else: #Take only those with full details mesh, title, abstract and year
         result_df = spark.sql("""
-            SELECT d.integer_id
+            SELECT d.integer_id, gp.PMID
             FROM george_pipeline gp
             JOIN exosome_dimensions d
             ON gp.doi = lower(d.doi)
             WHERE gp.has_abstract = 1 AND gp.has_title = 1 AND gp.has_mesh = 1 AND gp.has_year = 1
         """)
 
+        print("after db operation")
         write_df(result_df, jdbc_url, 'hm31.node_id_to_pmid_full', jdbc_properties)
 
 
@@ -56,12 +64,12 @@ def calculate_node_id_to_pmid_mapping(cen_nodes, george_df, full = False):
 
 
 
-
+#Write a df into the db
 def write_df(result, jdbc_url, table_name, jdbc_properties):
     result.repartition(50).write.jdbc(url=jdbc_url, table=table_name, mode="overwrite",
                                       properties=jdbc_properties)
 
-
+#read edges from a tsv file or from the database
 def read_EDGES(file_path, spark, read = True):
     if read == False:
         schema = StructType([StructField("first", IntegerType(), False), StructField("second", IntegerType(), False),StructField("weight", FloatType(), True)])
@@ -76,7 +84,7 @@ def read_EDGES(file_path, spark, read = True):
 
     return edges
 
-
+#Filter edges of CEN to a list of node_id1 node_id to but to those within dimensions and full metadata
 def filter_edges(edge, map):
     print('prior_count', edge.count())
 
@@ -105,6 +113,30 @@ def filter_edges(edge, map):
 
     return filtered_edges
 
+#Fitler nodes of cen to those with metadata
+def filter_nodes():
+    cen_raw = read_df(spark, jdbc_url, 'hm31.cen_raw', jdbc_properties)
+    map = read_df(spark, jdbc_url, 'hm31.node_id_to_pmid_full', jdbc_properties)
+
+    cen_raw = cen_raw.repartition(1)
+    map = map.repartition(1)
+
+
+    cen_raw.createOrReplaceTempView("cen_raw")
+    map.createOrReplaceTempView("map")
+
+    result_df = spark.sql("""
+            SELECT c.node_id, m.PMID
+            FROM cen_raw c
+            INNER JOIN map m
+            ON c.node_id = m.integer_id
+        """)
+
+    write_df(result_df, jdbc_url, 'hm31.cen_raw_nodes', jdbc_properties)
+
+
+
+#Read cen nodes from table and dump into csv
 def read_and_dump_cen_nodes(spark, jdbc_url, jdbc_properties):
     cen_raw = read_df(spark, jdbc_url, 'hm31.cen_raw', jdbc_properties)
     cen_raw.write.option("sep", "\t").csv('/home/hm31/step_1/data/reformatted.tsv', header=True, sep='\t', mode='overwrite')
@@ -112,26 +144,6 @@ def read_and_dump_cen_nodes(spark, jdbc_url, jdbc_properties):
 
 
 
-def create_CEN_intersection_table(spark, jdbc_url, jdbc_properties):
-    cr = read_CEN(nodes_address, spark)
-    cr.show()
-    paxn = read_df(spark, jdbc_url, 'hm31.pubmed_all_xmls_new', jdbc_properties)
-
-    paxn = paxn.repartition(1)  # Repartition 'paxn' into 100 partitions (adjust as needed)
-    cr = cr.repartition(1)  # Repartition 'cr' into 100 partitions (adjust as needed)
-
-    # print("PAXN", paxn.rdd.getNumPartitions())
-    # print("CRIS", cr.rdd.getNumPartitions())
-
-    result = cr.alias('cr').join(paxn.alias('paxn'), (func.col('cr.PMID') == func.col('paxn.pmid')), 'inner')
-
-    result.persist()
-    selected_columns = [func.col("paxn." + col_name) for col_name in paxn.columns]
-    result = result.select(*selected_columns)
-    result.repartition(50).write.jdbc(url=jdbc_url, table='hm31.xml_intersection', mode="overwrite",
-                                      properties=jdbc_properties)
-    result.show()
-    print("Hey", result.count())
 
 if __name__ == "__main__":
     nodes_address = '../data/reformatted.tsv'
@@ -157,17 +169,17 @@ if __name__ == "__main__":
     george_df = read_df(spark, jdbc_url, 'hm31.george_pipeline', jdbc_properties )
 
 
-
+    filter_nodes()
     #calculate_node_id_to_pmid_mapping(cen, george_df, full = False) #10484769
     #calculate_node_id_to_pmid_mapping(cen, george_df, full = True) #9304726
 
     #Obtain mapping
-    node_id_to_pmid_mapping = read_df(spark,jdbc_url,'hm31.node_id_to_pmid_full',jdbc_properties)
-
-    #Obtain edges
-    edges = read_EDGES(edges_address,spark)
-
-    filter_edges(edges,node_id_to_pmid_mapping)
+    # node_id_to_pmid_mapping = read_df(spark,jdbc_url,'hm31.node_id_to_pmid_full',jdbc_properties)
+    #
+    # #Obtain edges
+    # edges = read_EDGES(edges_address,spark)
+    #
+    # filter_edges(edges,node_id_to_pmid_mapping)
 
 
 

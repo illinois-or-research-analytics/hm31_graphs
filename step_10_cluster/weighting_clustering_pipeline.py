7d564f80c9195871ead8d7ac4231fd03667777e0
Hossein Mohasel Arjomandi <hm31@chackoge-serv01.cs.illinois.edu>
2024-01-06 19:19:02 -0600

    Passing parameters to optimizers

--- step_10_cluster/weighting_clustering_pipeline.py
+++ step_10_cluster/weighting_clustering_pipeline.py
@@ -9,6 +9,8 @@ import igraph as ig
 import  leidenalg as la
 import numpy as np
 import networkx as nx
+import pickle
+import numpy as np
 from pandarallel import pandarallel #error in   File "/home/hm31/step_1/venv/lib/python3.6/site-packages/pandarallel/progress_bars.py", line 8, in <module>
 #Made this change: https://github.com/qilingframework/qiling/commit/902e01beb94e2e27e50d1456e51e0ef99937aff1
 
@@ -87,6 +89,31 @@ def load_graphs( nx_path, ig_path):
     H_igraph = ig.read(ig_path)
     return G_networkx, H_igraph
 
+def test():
+    G = nx.erdos_renyi_graph(50, 0.5, seed=123, directed=False)
+    w = np.random.rand(len(G.edges()))
+
+    H = ig.Graph.from_networkx(G)
+    # partition =
+    # # print(partition)
+    # print(type(partition))
+
+    kwargs = {'resolution_parameter': 0.5}
+
+    # Create a CPMVertexPartition instance with additional parameters
+    partition = la.CPMVertexPartition(H, **kwargs)
+
+    result_partition = la.find_partition(H, la.CPMVertexPartition, weights=w, **kwargs)
+
+
+    # part = la.find_partition(graph=H, partition_type = la.ModularityVertexPartition, weights=w)
+
+    # print(part)
+    print(result_partition)
+    exit(0)
+
+
+
 
 def leiden(graph, leiden_partition, pandas_df):
 
@@ -94,13 +121,10 @@ def leiden(graph, leiden_partition, pandas_df):
     if leiden_partition=="Modularity":
         partition = la.ModularityVertexPartition
     elif leiden_partition=="CPM":
-        partition = la.CPMVertexPartition
+        partition = la.CPMVertexPartition(resolution_parameter=1)
 
-    # part = la.find_partition(graph, partition, weights=weights["weight"])
-    # part = la.find_partition(graph, partition)
-    # print(pandas_df)
     part = la.find_partition(graph, partition, weights=pandas_df['weight'])
-
+    #
     t2 = time.time()
     print(f'clustering {t2-t1}')
     return part
@@ -108,6 +132,8 @@ def leiden(graph, leiden_partition, pandas_df):
 
 
 if __name__ == '__main__':
+    test()
+
     # spark = SparkSession.builder \
     #     .appName("parquet_clustering") \
     #     .config("spark.executor.memory", "10g") \
@@ -131,14 +157,13 @@ if __name__ == '__main__':
     # print(df.tail())
     nw = [1,2,3,4,5,6,7]
     scale_factor = 1
-
     weights = []
 
     for i in nw:
             weights.append(i/sum(nw))
 
 
-
+    result_df = []
     t1 = time.time()
     result_df = apply_row_transformation(df, weights, scale_factor)
     t2 = time.time()
@@ -156,7 +181,7 @@ if __name__ == '__main__':
 
     print(f'Cluster: {t2-t1}')
 
-    mod = nx.community.modularity(G_nx, x)
+    mod = nx.community.modularity(G_nx, x, weight = None)
     t1 = time.time()
     print(f'Modularity: {t1-t2}')
 
@@ -169,6 +194,18 @@ if __name__ == '__main__':
     # clustering 429.36126685142517
     # Cluster: 429.36149644851685
     # Modularity: 183.89536952972412
+    #0.6535338908012208 with weights
+    #0.6506708581894841 without weights
+
+    with open('part.pickle', 'wb') as file:
+        pickle.dump(x, file)
+
+    print(type(x))
+    print(len(x))
+
+    for i in range(10):
+        print(len(x[i]))
+
 
 
 

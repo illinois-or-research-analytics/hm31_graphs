cca5a2b43e94a5cbbbf7792f786225e56b3b1d1f
Hossein Mohasel Arjomandi <hm31@chackoge-serv01.cs.illinois.edu>
2024-01-07 18:12:11 -0600

    Pipeline with CPM and summarization

--- step_10_cluster/weighting_clustering_pipeline.py
+++ step_10_cluster/weighting_clustering_pipeline.py
@@ -1,5 +1,6 @@
 #Lets switch to pandas world from Pyspark
 import argparse
+import os
 import time
 from tqdm import tqdm
 from pyspark.sql import SparkSession
@@ -8,8 +9,9 @@ import tables
 import igraph as ig
 import  leidenalg as la
 import numpy as np
+import json
 import networkx as nx
-import pickle
+import matplotlib.pyplot as plt
 import numpy as np
 from pandarallel import pandarallel #error in   File "/home/hm31/step_1/venv/lib/python3.6/site-packages/pandarallel/progress_bars.py", line 8, in <module>
 #Made this change: https://github.com/qilingframework/qiling/commit/902e01beb94e2e27e50d1456e51e0ef99937aff1
@@ -20,7 +22,6 @@ def convert_pyspark_features_to_pandas(spark, name):
 
     pandas_df = df.toPandas()
     pandas_df.to_hdf(name, key='data', mode='w')
-    print(pandas_df)
 
 def handle_arguments():
     parser = argparse.ArgumentParser(description='Example script to get a string argument.')
@@ -57,15 +58,86 @@ def handle_arguments():
     for i in input_array:
         weights.append(float(i)/float(addition))
 
-    print(weights)
 
 
     assert len(weights) == 7
 
-    print(weights)
-    print(scale)
 
     return weights, scale
+
+def record_clusterin_statistics(clustering_dict, load = False, file_name = None):
+
+    if load == False:
+
+        n_clusters = len(clustering_dict) - 1
+        cluster_statistics = np.zeros(n_clusters)
+        singleton = 0
+        total = 0
+
+        #Modularity is also stored in this dict
+        for key, value in clustering_dict.items():
+            if 'r' in key:
+                continue
+
+
+            cluster_statistics[int(key)] = len(value)
+            if len(value) == 1:
+                singleton += 1
+
+            total += len(value)
+
+
+
+        min_size = np.amin(cluster_statistics)
+        max_size = np.amax(cluster_statistics)
+        mean_size = np.mean(cluster_statistics)
+        median_size = np.median(cluster_statistics)
+        coverage = 1.0* (total - singleton)/total * 100
+
+        first_quant = np.quantile(cluster_statistics, .25)
+        third_quant = np.quantile(cluster_statistics, .75)
+
+        results_dict = {'min': min_size, 'max': max_size, 'mean': mean_size, 'median': median_size,
+                        'q1': first_quant, 'q3': third_quant, 'coverage': coverage, 'modularity': clustering_dict['Modularity']}
+
+        log_clusters = np.zeros_like(cluster_statistics)
+        for i in range(cluster_statistics.shape[0]):
+            log_clusters[i] = np.log10(cluster_statistics[i])
+
+        json_idx = file_name.index('json')
+        base_file_name = file_name[:json_idx-1].split('/')[-1]
+
+
+        plt.hist(log_clusters, bins=20, color='blue', alpha=0.7)
+        plt.title(f'Histogram of log cluster sizes {base_file_name}')
+        plt.xlabel('log cluster size')
+        plt.ylabel('N')
+        plt.grid(True)
+        # plt.show()
+
+        plt.savefig(f'files/results/{base_file_name}.png')
+
+
+        with open(f'files/results/{base_file_name}.json', 'w') as json_file:
+            json.dump(results_dict, json_file)
+
+
+
+
+
+
+    else:
+        with open(file_name, 'r') as file:
+           clustering_dict_loaded = json.load(file)
+
+        record_clusterin_statistics(clustering_dict_loaded, False, file_name)
+
+
+
+
+
+
+
 
 def apply_row_transformation(df, weights, scale):
     pandarallel.initialize(progress_bar=False) # initialize(36) or initialize(os.cpu_count()-1)
@@ -94,46 +166,68 @@ def test():
     w = np.random.rand(len(G.edges()))
 
     H = ig.Graph.from_networkx(G)
-    # partition =
-    # # print(partition)
-    # print(type(partition))
 
     kwargs = {'resolution_parameter': 0.5}
 
     # Create a CPMVertexPartition instance with additional parameters
-    partition = la.CPMVertexPartition(H, **kwargs)
 
     result_partition = la.find_partition(H, la.CPMVertexPartition, weights=w, **kwargs)
+    # result_partition = la.find_partition(H, la.ModularityVertexPartition, weights=w, **kwargs)
 
 
     # part = la.find_partition(graph=H, partition_type = la.ModularityVertexPartition, weights=w)
 
-    # print(part)
-    print(result_partition)
+
+    clustering = {}
+
+
+    for idx, cluster in enumerate(result_partition):
+        clustering[idx] = cluster
+
+
     exit(0)
 
 
 
 
-def leiden(graph, leiden_partition, pandas_df):
+def leiden(graph, leiden_partition, pandas_df, resolution_parameter = None):
 
     t1 = time.time()
     if leiden_partition=="Modularity":
-        partition = la.ModularityVertexPartition
-    elif leiden_partition=="CPM":
-        partition = la.CPMVertexPartition(resolution_parameter=1)
+        part = la.find_partition(graph, la.ModularityVertexPartition, weights=pandas_df['weight'])
 
-    part = la.find_partition(graph, partition, weights=pandas_df['weight'])
-    #
+    elif leiden_partition=="CPM":
+        kwargs = {'resolution_parameter': resolution_parameter}
+        part = la.find_partition(graph, la.CPMVertexPartition, weights=pandas_df['weight'], **kwargs)
+
+
+
+
+    # Create a CPMVertexPartition instance with additional parameters
+
+
     t2 = time.time()
     print(f'clustering {t2-t1}')
     return part
 
+#Summarize clustering statustics
+def summarize(address = 'files/clusterings/' ):
+    files = os.listdir(address)
+
+    for file in files:
+        if '.json' in file:
+            record_clusterin_statistics(None, True, f'{address}{file}')
+
+
+
+
 
 
 if __name__ == '__main__':
-    test()
+    # record_clusterin_statistics(None, True, f'files/clusterings/Modularity.json') 194233
+    # exit(0)
 
+    # test()
     # spark = SparkSession.builder \
     #     .appName("parquet_clustering") \
     #     .config("spark.executor.memory", "10g") \
@@ -150,11 +244,8 @@ if __name__ == '__main__':
     ig_path = f"files/graphs/base_ig.pickle"
 
     df = pd.read_hdf(name, key='data')
-    print(df.columns)
 
-    print(len(df))
 
-    # print(df.tail())
     nw = [1,2,3,4,5,6,7]
     scale_factor = 1
     weights = []
@@ -176,45 +267,70 @@ if __name__ == '__main__':
     t1 = time.time()
     print(f'Load graphs: {t1 - t2}')
 
-    x = leiden(H_ig, 'Modularity', result_df)
-    t2 = time.time()
 
-    print(f'Cluster: {t2-t1}')
+    #def leiden(graph, leiden_partition, pandas_df, resolution_parameter = None):
+    resolution_values = [0.95, 0.75, 0.50, 0.25, 0.05, 0.01, 0.001, 0.0001]
 
+    for resolution_value in resolution_values:
+        break
+        x = leiden(H_ig, 'CPM', result_df, resolution_value)
+        # t2 = time.time()
+
+
+        mod = nx.community.modularity(G_nx, x, weight = None)
+        # t1 = time.time()
+
+
+
+        clsutering_dict = {}
+
+        for idx, cluster in enumerate(x):
+            clsutering_dict[idx] = cluster
+
+
+        clsutering_dict['Modularity'] = mod
+
+        with open(f'files/clusterings/CPM_{resolution_value}.json', 'w') as json_file:
+            json.dump(clsutering_dict, json_file)
+
+        # t2 = time.time()
+
+        # print(f'saving {t2-t1}')
+
+
+    x = leiden(H_ig, 'Modularity', result_df, resolution_value)
     mod = nx.community.modularity(G_nx, x, weight = None)
-    t1 = time.time()
-    print(f'Modularity: {t1-t2}')
-
-    print(mod)
 
 
-    # Calculate weights: 61.890968561172485
-    # Load graphs: 161.23537063598633
+    clsutering_dict = {}
 
-    # clustering 429.36126685142517
-    # Cluster: 429.36149644851685
-    # Modularity: 183.89536952972412
-    #0.6535338908012208 with weights
-    #0.6506708581894841 without weights
-
-    with open('part.pickle', 'wb') as file:
-        pickle.dump(x, file)
-
-    print(type(x))
-    print(len(x))
-
-    for i in range(10):
-        print(len(x[i]))
+    for idx, cluster in enumerate(x):
+        clsutering_dict[idx] = cluster
 
 
+    clsutering_dict['Modularity'] = mod
+
+    with open(f'files/clusterings/Unweighted.json', 'w') as json_file:
+        json.dump(clsutering_dict, json_file)
 
 
+    x = leiden(H_ig, 'Modularity', result_df, resolution_value)
+    mod = nx.community.modularity(G_nx, x, weight = None)
 
 
+    clsutering_dict = {}
+
+    for idx, cluster in enumerate(x):
+        clsutering_dict[idx] = cluster
 
 
+    clsutering_dict['Modularity'] = mod
+
+    with open(f'files/clusterings/Modularity.json', 'w') as json_file:
+        json.dump(clsutering_dict, json_file)
 
 
+    summarize('files/clusterings/')
 
 
 
